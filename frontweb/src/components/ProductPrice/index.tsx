10966eb12720a8b8c15ceca8b9be08819349877d
moduretick <robertoreboucasdemelo@gmail.com>
2021-11-05 23:03:40 -0300

    Add Navbar

---
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+const ProductPrice = () => {
+  return (
+    <>
+      <div className="product-price-container">
+        <span>R$</span>
+        <h3>2345.78</h3>
+      </div>
+    </>
+  );
+};
+
+export default ProductPrice;
